bdcb6d5d6ee7d904687db6b46eac8a6ac18e12f9
Beatris Dimitrova <Beatris.Dimitrovaaa@gmail.com>
2023-01-18 22:51:59 +0200

    test: Verify the handling of name input

--- cypress/e2e/test-suite.cy.js
+++ cypress/e2e/test-suite.cy.js
@@ -7,4 +7,9 @@ describe("Test suite", () => {
   it("Check if the title is correct", () => {
     cy.get(".title").should("have.text","Registration Info");
   });
+
+  it("Verify the handling of name input", () => {
+    cy.get('#registration-form > :nth-child(1) > .input--style-1').clear().type('John').should('have.value', 'John');
+  });
+
 });
