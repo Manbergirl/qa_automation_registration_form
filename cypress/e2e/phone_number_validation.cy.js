cbb3041ef3bcfa2385ec1d0a6e814653d43b0228
Beatris Dimitrova <Beatris.Dimitrovaaa@gmail.com>
2023-01-20 16:19:10 +0200

    test: Birthday randomly selcted and new user is between 18 and 75 years old

--- cypress/e2e/phone_number_validation.cy.js
+++ cypress/e2e/phone_number_validation.cy.js
@@ -16,7 +16,27 @@ it('Phone number corresponds to the following schema', () => {
   cy.get('#registration-form .rs-select2 select.prefix').select(prefix, {force: true});
   cy.get('#registration-form > :nth-child(3) > :nth-child(2) > .input-group > .input--style-1').type(phoneNumber);
 });
+
+it('Birthday is randomly selcted and new user is between 18 and 75 years old',()=> {
+  const minAge = 18;
+  const maxAge = 75;
+  const age = Cypress._.random(minAge, maxAge);
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  const minMonth = 0;
+  const maxMonth = 12;
+  const minDay = 1;
+  const maxDay = 28;
+  const randomMonth = Cypress._.random(minMonth,maxMonth);
+  const randomDay = Cypress._.random(minDay,maxDay);
+  const minCalendarNumber = 0;
+  const maxCalendarNumber = 10;
+  
+  const randomDOB = new Date(currentYear - age, randomMonth, randomDay);
+  cy.get('#registration-form .js-btn-calendar').type(randomDOB.toISOString().slice(minCalendarNumber,maxCalendarNumber));
+})
    
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
