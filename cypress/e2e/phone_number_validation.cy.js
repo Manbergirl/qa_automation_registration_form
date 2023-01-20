c7b2e752ea6d07acc8b73370843f69f0bee13686
Beatris Dimitrova <Beatris.Dimitrovaaa@gmail.com>
2023-01-20 15:22:32 +0200

    test: Phone number corresponds to the following schema

--- cypress/e2e/phone_number_validation.cy.js
+++ cypress/e2e/phone_number_validation.cy.js
@@ -1,10 +1,20 @@
-// const { it } = require("node:test");
+// const { type } = require("os");
+beforeEach(() => {
+  cy.viewport(1024, 768);
+  cy.visit("http://localhost:3000");
+});
 
-describe('Phone number validation', () => {
-  beforeEach(() => {
-    cy.viewport(1024, 768);
-    cy.visit("http://localhost:3000");
-  }); 
+it('Phone number corresponds to the following schema', () => {
+  const prefix = "+359";
+  const minMobOperatorNumber = 882;
+  const maxMobOperatorNumber = 895;
+  const minNumber = 100000;
+  const maxNumber = 999999;
+  const mobileOperator = randomIntFromInterval(minMobOperatorNumber, maxMobOperatorNumber);
+  const number = randomIntFromInterval(minNumber, maxNumber)
+  const phoneNumber = `${prefix}${mobileOperator}${number}`;
+  cy.get('#registration-form .rs-select2 select.prefix').select(prefix, {force: true});
+  cy.get('#registration-form > :nth-child(3) > :nth-child(2) > .input-group > .input--style-1').type(phoneNumber);
 });
    
 function randomIntFromInterval(min, max) {
